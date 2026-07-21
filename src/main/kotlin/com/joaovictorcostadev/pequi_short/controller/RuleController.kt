da0e5c9082239aa7fff504d7363391113b86006c
Joao Victor Costa <joaovictorcosta.dev@gmail.com>
2026-07-21 15:02:19 -0300

    feat: add preAuthorize in create rule method

--- src/main/kotlin/com/joaovictorcostadev/pequi_short/controller/RuleController.kt
+++ src/main/kotlin/com/joaovictorcostadev/pequi_short/controller/RuleController.kt
@@ -6,6 +6,7 @@ import com.joaovictorcostadev.pequi_short.dto.rule.RuleResponseDto
 import com.joaovictorcostadev.pequi_short.service.RuleService
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class RuleController (val ruleService: RuleService) {
 
+    @PreAuthorize("hasAuthority('RULE_CREATE')")
     @PostMapping("api/rule/save")
     fun save(@Valid @RequestBody ruleRequest: RuleRequestDto ) :  ResponseEntity<ResponseDto<RuleResponseDto>> {
         return ruleService.save(ruleRequest);
